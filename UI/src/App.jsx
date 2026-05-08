bdeb468eaff8915076803db4314dbde916215885
azizelkateb <azizkateb69@gmail.com>
2026-05-08 16:17:06 +0100

    fix router for github pages

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import { useTranslation } from 'react-i18next'
 import { motion } from 'framer-motion'
--- UI/vite.config.js
+++ UI/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Qoran/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 600,
@@ -31,3 +32,4 @@ export default defineConfig({
     },
   },
 })
+
